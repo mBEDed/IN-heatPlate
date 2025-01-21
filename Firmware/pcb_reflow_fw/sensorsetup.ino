a1e252f4e20095e14ee9978e0530bd061bbbca5e
Anand Pandey <146065039+anandpandeyx@users.noreply.github.com>
2025-01-21 18:12:52 +0530

    Delete Firmware/pcb_reflow_fw directory

--- Firmware/pcb_reflow_fw/sensorsetup.ino
+++
@@ -1,9 +0,0 @@
-inline void setupSensors() {
-    sensors.begin();
-    sensor_count = sensors.getDeviceCount();
-    debugprint("Looking for sensors, found: ");
-    debugprintln(sensor_count);
-    for (int i = 0; i < min(sensor_count, sizeof(temp_addresses)); i++) {
-        sensors.getAddress(temp_addresses[i], i);
-    }
-}
